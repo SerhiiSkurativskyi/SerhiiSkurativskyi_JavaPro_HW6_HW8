2353c682026b240d2503a3a0cd67e220520687f1
Serhii Skurativskyi <serhii.skurativskyi@gmail.com>
2023-02-20 21:13:01 +0200

    save commit 2

--- src/main/java/one/FindOccurrence.java
+++ src/main/java/one/FindOccurrence.java
@@ -4,16 +4,16 @@ public class FindOccurrence {
     private String name;
     private int occurrence;
 
+    public FindOccurrence(String name, int occurrence) {
+        this.name = name;
+        this.occurrence = occurrence;
+    }
+
     public String getName() {
         return name;
     }
 
     public int getOccurrence() {
         return occurrence;
-    }
-
-    public FindOccurrence(String name, int occurrence) {
-        this.name = name;
-        this.occurrence = occurrence;
     }
 }
--- src/main/java/phonebook/PhoneBook.java
+++ src/main/java/phonebook/PhoneBook.java
@@ -1,43 +1,44 @@
 package phonebook;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
-    public class PhoneBook {
-        private final List<Record> records;
+public class PhoneBook {
+    private final List<Record> records;
 
-        public PhoneBook() {
-            this.records = new ArrayList<>();
-        }
-
-        public List<Record> getRecords() {
-            return records;
-        }
-
-        public void add(Record record) {
-            records.add(record);
-        }
-
-        public Record find(String name) {
-            for (Record record : this.records) {
-                if (record.getName().equals(name)) {
-                    return record;
-                }
-            }
-            return null;
-        }
-
-        public List<Record> findAll(String name) {
-            List<Record> foundRecords = new ArrayList<>();
-            for (Record record : this.records) {
-                if (record.getName().equals(name)) {
-                    foundRecords.add(record);
-                }
-            }
-            if (foundRecords.isEmpty()) {
-                return null;
-            }
-            return foundRecords;
-        }
-
+    public PhoneBook() {
+        this.records = new ArrayList<>();
     }
+
+    public List<Record> getRecords() {
+        return records;
+    }
+
+    public void add(Record record) {
+        records.add(record);
+    }
+
+    public Record find(String name) {
+        for (Record record : this.records) {
+            if (record.getName().equals(name)) {
+                return record;
+            }
+        }
+        return new Record("", "");
+    }
+
+    public List<Record> findAll(String name) {
+        List<Record> foundRecords = new ArrayList<>();
+        for (Record record : this.records) {
+            if (record.getName().equals(name)) {
+                foundRecords.add(record);
+            }
+        }
+        if (foundRecords.isEmpty()) {
+            return List.of();
+        }
+        return foundRecords;
+    }
+
+}
