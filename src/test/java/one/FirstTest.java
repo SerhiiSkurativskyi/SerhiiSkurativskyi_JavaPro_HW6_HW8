6e8be3e5bb7861561846308bc837b885f72d484c
Serhii Skurativskyi <serhii.skurativskyi@gmail.com>
2023-02-20 19:15:40 +0200

    checkout

--- src/test/java/one/FirstTest.java
+++ src/test/java/one/FirstTest.java
@@ -1,13 +1,13 @@
-import one.FindOccurrence;
-import one.First;
+package one;
 import org.junit.jupiter.api.Test;
-import org.testng.Assert;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.testng.Assert.assertEquals;
 
-public class TestFirst {
+
+public class FirstTest {
 
     @Test
     public void testCountOccurrence() {
@@ -27,7 +27,7 @@ public class TestFirst {
         sourceList.add("boy");
         sourceList.add("top");
         String sourceString = "target";
-        Assert.assertEquals(3, first.countOccurrence(sourceList, sourceString));
+        assertEquals(3, first.countOccurrence(sourceList, sourceString));
     }
 
 
@@ -35,15 +35,15 @@ public class TestFirst {
     public void testToList() {
         Integer[] intArray = {11, 2, 3, 5, 58, 7, 7, 874};
         List<Integer> intList = First.toList(intArray);
-        Assert.assertEquals(intList.size(), 8);
-        Assert.assertEquals(intList.get(0), Integer.valueOf(11));
-        Assert.assertEquals(intList.get(4), Integer.valueOf(58));
+        assertEquals(intList.size(), 8);
+        assertEquals(intList.get(0), Integer.valueOf(11));
+        assertEquals(intList.get(4), Integer.valueOf(58));
 
         String[] stringArray = {"bot", "boy", "cat", "hat", "map", "dry", "top", "man"};
         List<String> targetList = First.toList(stringArray);
-        Assert.assertEquals(targetList.size(), 8);
-        Assert.assertEquals(targetList.get(0), String.valueOf("bot"));
-        Assert.assertEquals(targetList.get(4), String.valueOf("map"));
+        assertEquals(targetList.size(), 8);
+        assertEquals(targetList.get(0), String.valueOf("bot"));
+        assertEquals(targetList.get(4), String.valueOf("map"));
     }
 
     @Test
@@ -51,7 +51,7 @@ public class TestFirst {
         First first = new First();
         List<Integer> sourceList = Arrays.asList(5, 2, 2, 5, 1, 4, 2, 2, 7, 2, 7);
         List<Integer> targetList = Arrays.asList(5, 2, 1, 4, 7);
-        Assert.assertEquals(targetList, first.findUnique(sourceList));
+        assertEquals(targetList, first.findUnique(sourceList));
     }
 
     @Test
@@ -59,7 +59,7 @@ public class TestFirst {
         First first = new First();
         List<String> sourceList = Arrays.asList("boy", "boy", "boy", "boy", "cat", "man", "man", "dog", "dog");
         List<String> targetList = Arrays.asList("boy: 4", "cat: 1", "man: 2", "dog: 2");
-        Assert.assertEquals(targetList, first.calcOccurrence(sourceList));
+        assertEquals(targetList, first.calcOccurrence(sourceList));
     }
 
     @Test
@@ -71,10 +71,10 @@ public class TestFirst {
         targetList.add(new FindOccurrence("man", 2));
         targetList.add(new FindOccurrence("dog", 2));
         List<FindOccurrence> result = new First().findOccurrence(sourceList);
-        Assert.assertEquals(targetList.size(), result.size());
+        assertEquals(targetList.size(), result.size());
         for (int i = 0; i < targetList.size(); i++) {
-            Assert.assertEquals(targetList.get(i).getName(), result.get(i).getName());
-            Assert.assertEquals(targetList.get(i).getOccurrence(), result.get(i).getOccurrence());
+            assertEquals(targetList.get(i).getName(), result.get(i).getName());
+            assertEquals(targetList.get(i).getOccurrence(), result.get(i).getOccurrence());
         }
     }
 }
--- src/test/java/phonebook/PhoneBookTest.java
+++ src/test/java/phonebook/PhoneBookTest.java
@@ -1,15 +1,16 @@
-import org.junit.jupiter.api.Test;
-import org.testng.Assert;
-import phonebook.PhoneBook;
-import phonebook.Record;
+package phonebook;
 
-public class TestPhoneBook {
+import org.junit.jupiter.api.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class PhoneBookTest {
     @Test
     public void testAdd() {
         PhoneBook newPerson = new PhoneBook();
         newPerson.add(new Record("Tommy Shelby", "+380979839855"));
-        Assert.assertEquals("Tommy Shelby", newPerson.getRecords().get(0).getName());
-        Assert.assertEquals("+380979839855", newPerson.getRecords().get(0).getPhoneNumber());
+        assertEquals("Tommy Shelby", newPerson.getRecords().get(0).getName());
+        assertEquals("+380979839855", newPerson.getRecords().get(0).getPhoneNumber());
     }
 
     @Test
@@ -18,7 +19,7 @@ public class TestPhoneBook {
         newPerson.add(new Record("Tommy Shelby", "+380979839855"));
         newPerson.add(new Record("Arthur Shelby", "+380972587468"));
         newPerson.add(new Record("John Shelby", "+380979874521"));
-        Assert.assertEquals("+380972587468", newPerson.find("Arthur Shelby").getPhoneNumber());
+        assertEquals("+380972587468", newPerson.find("Arthur Shelby").getPhoneNumber());
     }
 
     @Test
@@ -30,8 +31,8 @@ public class TestPhoneBook {
         newPerson.add(new Record("John Shelby", "+380979874521"));
         newPerson.add(new Record("Tommy Shelby", "+380978549787"));
 
-        Assert.assertEquals(newPerson.findAll("Tommy Shelby").get(0).getPhoneNumber(), "+380979839855");
-        Assert.assertEquals(newPerson.findAll("Tommy Shelby").get(1).getPhoneNumber(), "+380976548964");
-        Assert.assertEquals(newPerson.findAll("Tommy Shelby").get(2).getPhoneNumber(), "+380978549787");
+        assertEquals(newPerson.findAll("Tommy Shelby").get(0).getPhoneNumber(), "+380979839855");
+        assertEquals(newPerson.findAll("Tommy Shelby").get(1).getPhoneNumber(), "+380976548964");
+        assertEquals(newPerson.findAll("Tommy Shelby").get(2).getPhoneNumber(), "+380978549787");
     }
 }
