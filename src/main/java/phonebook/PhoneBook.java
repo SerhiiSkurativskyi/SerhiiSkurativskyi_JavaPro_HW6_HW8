cc4d9e73f19f193d9cc3a5e08eafe149ccee59f7
SerhiiSkurativskyi <115039893+SerhiiSkurativskyi@users.noreply.github.com>
2023-03-11 16:26:55 +0300

    Gradle actions (#6)
    * little changes
    
    * actions

--- src/main/java/phonebook/PhoneBook.java
+++ src/main/java/phonebook/PhoneBook.java
@@ -1,44 +1,43 @@
 package phonebook;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class PhoneBook {
     private final List<Record> records;
 
-    public PhoneBook() {
-        this.records = new ArrayList<>();
-    }
+        public PhoneBook() {
+            this.records = new ArrayList<>();
+        }
 
-    public List<Record> getRecords() {
-        return records;
-    }
+        public List<Record> getRecords() {
+            return records;
+        }
 
-    public void add(Record record) {
-        records.add(record);
-    }
+        public void add(Record record) {
+            records.add(record);
+        }
 
-    public Record find(String name) {
-        for (Record record : this.records) {
-            if (record.getName().equals(name)) {
-                return record;
+        public Record find(String name) {
+            for (Record record : this.records) {
+                if (record.getName().equals(name)) {
+                    return record;
+                }
             }
+            return null;
         }
-        return new Record("", "");
-    }
 
-    public List<Record> findAll(String name) {
-        List<Record> foundRecords = new ArrayList<>();
-        for (Record record : this.records) {
-            if (record.getName().equals(name)) {
-                foundRecords.add(record);
+        public List<Record> findAll(String name) {
+            List<Record> foundRecords = new ArrayList<>();
+            for (Record record : this.records) {
+                if (record.getName().equals(name)) {
+                    foundRecords.add(record);
+                }
             }
+            if (foundRecords.isEmpty()) {
+                return null;
+            }
+            return foundRecords;
         }
-        if (foundRecords.isEmpty()) {
-            return List.of();
-        }
-        return foundRecords;
-    }
 
-}
+    }
